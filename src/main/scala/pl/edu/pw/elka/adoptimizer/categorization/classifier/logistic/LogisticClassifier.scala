1fa281938a0320100e54bec7175abe1bc6938124
Paul <paul.dax2009@gmail.com>
2018-01-05 00:21:32 +0100

    tfidf vectorization

--- src/main/scala/pl/edu/pw/elka/adoptimizer/categorization/classifier/logistic/LogisticClassifier.scala
+++ src/main/scala/pl/edu/pw/elka/adoptimizer/categorization/classifier/logistic/LogisticClassifier.scala
@@ -2,15 +2,30 @@ package pl.edu.pw.elka.adoptimizer.categorization.classifier.logistic
 
 import pl.edu.pw.elka.adoptimizer.categorization.classifier.TextClassifier
 import pl.edu.pw.elka.adoptimizer.categorization.model.Sample
+import pl.edu.pw.elka.adoptimizer.categorization.tokenizer.NgramTokenizer
+import pl.edu.pw.elka.adoptimizer.categorization.vectorizer.TfIdfVectorizer
 import weka.classifiers.functions.Logistic
-import weka.core.DenseInstance
+import weka.core.{DenseInstance, Instances}
 
-class LogisticClassifier extends TextClassifier {
-  val lr = new Logistic()
+class LogisticClassifier(maxNgrams: Int = 1) extends TextClassifier {
+  private val lr = new Logistic()
+  private var tfIdfVectorizer: TfIdfVectorizer = _
 
   override def classify(sample: Sample): Double = ???
 
-  override def fit(samples: List[Sample]): Unit = ???
+  override def fit(samples: List[Sample]): Unit = {
+    tfIdfVectorizer = new TfIdfVectorizer(samples.map(_.content), NgramTokenizer(1 to maxNgrams))
+    val trainingSet = samples.map(sample => (tfIdfVectorizer.vectorize(sample.content), sample.category))
+    .map(sample => {
+      val instance = new DenseInstance(tfIdfVectorizer.numFeatures)
+
+      sample._1.zipWithIndex.foreach(weight => instance.setValue(weight._2, weight._1))
+      instance.setClassValue(sample._2)
+
+      instance
+    })
+    //lr.buildClassifier(trainingSet)
+  }
 
   override def save(): Any = ???
 
---
+++ src/main/scala/pl/edu/pw/elka/adoptimizer/categorization/vectorizer/TfIdfVectorizer.scala
@@ -0,0 +1,16 @@
+package pl.edu.pw.elka.adoptimizer.categorization.vectorizer
+
+import pl.edu.pw.elka.adoptimizer.categorization.tokenizer.{NgramTokenizer, TfIdfTransformer}
+
+class TfIdfVectorizer(corpus: List[String], tokenizer: NgramTokenizer = NgramTokenizer(1 to 1)) {
+  private val ngrams = corpus.map(tokenizer.tokenize)
+  private val features = ngrams.flatMap(_.keys).distinct
+  private val transformer = new TfIdfTransformer(ngrams)
+
+  def vectorize(text: String): List[Double] = {
+    val counts = transformer.tfidf(tokenizer.tokenize(text))
+    features.map(counts.getOrElse(_, 0))
+  }
+
+  val numFeatures: Int = features.length
+}
