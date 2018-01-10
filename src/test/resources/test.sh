b6ff2ee2149e9d491c74e4077aae336fa62155dd
Paul <P.Chudzik@stud.elka.pw.edu.pl>
2018-01-10 11:20:58 +0100

    update test script

--- src/test/resources/test.sh
+++ src/test/resources/test.sh
@@ -2,5 +2,10 @@ for file in `find . -name "*.json"`
 do
     echo `cat $file | grep \"category\":`
     echo `curl -X POST -H "Content-Type: application/json" -d @$file http://localhost:8080/ad` > "$file".html
-    xdg-open "$file".html
+    if command -v xdg-open
+    then
+       xdg-open "$file".html
+    else
+       open "$file".html    
+    fi
 done
